864acf6ca60e554aea845f7782199c757005015e
Cara Brook <cbrook04@gmail.com>
2020-01-05 08:56:46 +0300

    updates

--- E2M2-2020/Tutorials/E2M2_Bootcamp_IfElse/Loop_ifelse_functions.R
+++ E2M2-2020/Tutorials/E2M2_Bootcamp_IfElse/Loop_ifelse_functions.R
@@ -10,12 +10,16 @@ data<-diamonds
 
 head(data)
 
-#we want to check through this dataset and if a diamond has cara higher than 0.8, we say it is high quality
-#we can check it manually and print the carat of each diamond in our dataset
+#we want to check through he first 30 elements of this dataset we want the carat [weight], the price and the difference in price with the following diamond
+#of the diamond and if a diamond has carats higher than 0.8, we say it is high quality
+#we can check it manually and print the values and make the calculations ourselves in our dataset
 
-for (i in 1:30){ ## This line creates a variable "i" that is 20 lines long
-  print(data$carat[i])#Here we loop through and print each carat value of "i" as specified in the initial loop
+for (i in 1:30){ ## This line creates a variable "i" that is 30 lines long
+  print(paste(data$carat[i], data$price[i], (data$price[i+1]-data$price[i])))
+  #Here we loop through and print each carat value of "i" 
+  #as specified in the initial loop
   }
+
 
 
 #############################################################################
@@ -53,24 +57,47 @@ for (i in 1:length(data$carat)){
 
 
 
-
-#now what if we want to automate all of this and make it so that we only provide the carat and cut and it spits out those numbers?
+summary(as.factor(qc))
+#now what if we want to automate all of this and make it so that we only provide the "carat" and "cut" and it spits out those numbers?
 
 q.check<-function(car,cu){
+  qual<-c()
   for (i in 1:length(data$carat)){
     if(data$carat[i]>=car&data$cut[i]==cu){
-      qc<-append(qc,"high")}else{
-        qc<-append(qc,"low-medium")}
+      qual<-append(qual,"high")}else{
+        qual<-append(qual,"low-medium")}
   }
-  return(qc)
+  return(summary(as.factor(qual)))
 }
-q.check(2.0,"Premium")
+q.check(3.0,"Premium")
 
-#### finally we can write our own function to add this to our initial data frame
+######
+#enough about diamonds let's continue writing the function about the coin toss
 
-add.df<-function(df,vect){
-  new.df<-cbind(df,vect)
-  return(new.df)
+coin<-function(n,p)# we created a function called coin() where, we would need to specify the number of toss n and the probability of getting tail
+{
+  #the function will generate n numbers (either 0 or 1) with a p prob of getting tails.
+  Tail<-rbinom(n,1,p)
+  #count number of Tails
+  numTail<-sum(Tail)
+  return(numTail)
 }
-data<-add.df(data,qc)
-summary(data$vect)
+
+#let's try it out with a various number of toss and probabilities
+coin(1000,0.5)
+
+#### now we recruit x friends to help with an experiment
+reps<-replicate(5000,coin(10,0.5)) #5000 of my best friends each toss the coin 10 times
+hist(reps)
+#what is the pobability of getting exactly 5 tails 
+sum(reps==5)
+
+for (i in 1:10) {print (i)}
+
+##can you write a for loop to have the probabilities of getting exactly 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 tails
+
+for(i in 1:10){
+  print(sum(reps==i)/5000)}
+
+reps0.3<-replicate(5000,coin(10,0.3)) #5000 of my best friends each toss the coin 10 times but this time their coin is weighted
+hist(reps0.3, add=T)
---
+++ E2M2-2020/Tutorials/E2M2_Bootcamp_IfElse/exercise.R
@@ -0,0 +1,17 @@
+#### write a function that will calculate the area of a circle when you give it's radius
+
+
+
+
+
+
+
+
+####Now, add a for-loop to obtain the area of circles that have a range of radius
+
+
+
+
+
+
+#### finally if this area exceeds a limit l we want to print "circle is too big"
